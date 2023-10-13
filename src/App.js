d76503981db418a639fbbf215a0cbb72a78ad230
D-mitry12344 <114662273+D-mitry12344@users.noreply.github.com>
2023-10-13 14:09:41 +0300

    update react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+let people = [{p:'123', id:1},{p:'123', id:2}]
+
+
 function handlerClick(e) {
     if(e.target.className){
       e.target.className ='';
@@ -11,40 +14,52 @@ function handlerClick(e) {
 }
 
 
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-];
-
-
-
 function DeleteClick(e){
-  console.log(e.target.parentElement.firstChild);
+  for ( let i=0 ; i < people.length; i++){
+     if(people[i].p === e.target.parentElement.firstChild.textContent){
+      people.splice(i,i)
+      e.target.parentElement.remove()
+      return people
+    }
+  }
 }
 
 
 
-const listItems = people.map(person =>
-    <li onClick={handlerClick}>
-        <p>{person}</p>
+
+function addClick(e){
+  let obg = {
+    p: String(e.target.parentElement.firstChild.value),
+    id: people.length+1
+  }
+  people.push(obg);
+  console.log(people)
+  App()
+}
+
+
+function List(){
+  let listItems = people.map(person =>
+    <li key={person.id} onClick={handlerClick}>
+        <p>{person.p}</p>
         <button onClick={DeleteClick}>X</button>
     </li>
-
   );
+  return <ul>{listItems}</ul>
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
       <div>
         <input type="text"/>
-        <button>ADD</button>
+        <button onClick={addClick}>ADD</button>
       </div>
       <div>
-        <ul >{listItems}</ul>;
-
+        <List />
       </div>
     </div>
   );
